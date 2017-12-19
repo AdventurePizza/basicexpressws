6d93afc15b431a212c7aeb3e3fa9d33d50108424
Sharat Gajulapalli <blitzkreig@gmail.com>
2017-12-19 15:35:25 -0500

    some updated db code

--- server.js
+++ server.js
@@ -6,21 +6,33 @@ var express = require('express'),
   bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
-mongoose.Promise = global.Promise;
+//mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://forkchat:adventurecorp1@ds059207.mlab.com:59207/Tododb');
 
-mongoose.createConnection('mongodb://forkchat:adventurecorp1@ds059207.mlab.com:59207/forkchat', {
+/*var promise = mongoose.createConnection('mongodb://forkchat:adventurecorp1@ds059207.mlab.com:59207/forkchat', {
   useMongoClient: true,
-  /* other options */
+});*/
+mongoose.connect('mongodb://forkchat:adventurecorp1@ds059207.mlab.com:59207/forkchat', { useMongoClient:
+true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('openUri', function() {
+// we're connected!
 });
+
+/*promise.then(function(db) {
+   console.log('Got here');
+});*/
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-/*app.use(function(req, res) {
+/*
+app.use(function(req, res) {
   console.log('Entered this block');
   res.status(404).send({url: req.originalUrl + ' not found'})
-});*/
+});
+*/
 
 var routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
